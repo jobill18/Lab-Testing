094c774a4fdedd106fc53ec6e08bbb104b22c8a7
Joseph Billings <josephbillings.gb@gmail.com>
2024-07-10 15:10:18 -0600

    created tests for the Wordle method appendGuess, isSolved, shouldEndGame

--- __tests__/wordle.test.js
+++ __tests__/wordle.test.js
@@ -1,9 +1,11 @@
 import { jest } from "@jest/globals";
 
+let mockIsWord = jest.fn(() => true);
+
 jest.unstable_mockModule("../src/words.js", () => {
   return {
     getWord: jest.fn(() => "APPLE"),
-    isWord: jest.fn(() => true),
+    isWord: mockIsWord,
   };
 });
 
@@ -66,5 +68,68 @@ describe("test build guess from word", () => {
       letter: "Z",
       status: "ABSENT",
     });
+  });
+});
+
+describe("test Wordle.appendGuess", () => {
+  test("throws error if no more guesses are allowed", () => {
+    const newWordleNoGuess = new Wordle(1);
+    expect(() => {
+      newWordleNoGuess.appendGuess("WORDS");
+      newWordleNoGuess.appendGuess("WORDS");
+    }).toThrow("No more guesses allowed");
+  });
+  test("throws error if guess is not length of 5", () => {
+    const newWordleNoLong = new Wordle();
+    expect(() => {
+      newWordleNoLong.appendGuess("WORRIES");
+    }).toThrow("Guess must be of length 5");
+  });
+  test("throws error if the guess is not a word", () => {
+    const newWordleNoWord = new Wordle();
+    mockIsWord.mockReturnValueOnce(false);
+    expect(() => {
+      newWordleNoWord.appendGuess("WORRY");
+    }).toThrow("Guess must be a word");
+  });
+  test("increments the current guess", () => {
+    const newWordleNoWord = new Wordle();
+    newWordleNoWord.appendGuess("12312");
+    expect(newWordleNoWord.currGuess).toEqual(1);
+  });
+});
+
+describe("Test Wordle.isSolved", () => {
+  test("returns true if the latest guess is the correct word", () => {
+    const newWordle = new Wordle();
+    newWordle.appendGuess("APPLE");
+    expect(newWordle.isSolved()).toBe(true);
+  });
+  test("returns false if the latest guess is not the correct word", () => {
+    const newWordle = new Wordle();
+    newWordle.appendGuess("GUESS");
+    expect(newWordle.isSolved()).toBe(false);
+  });
+});
+
+describe("Test Wordle.shouldEndGame", () => {
+  test("returns true if the latest guess is the correct word", () => {
+    const newWordle = new Wordle();
+    newWordle.appendGuess("APPLE");
+    expect(newWordle.shouldEndGame()).toBe(true);
+  });
+  test("returns true if there are no more guesses left", () => {
+    const newWordle = new Wordle(1);
+    newWordle.appendGuess("GUESS");
+    expect(newWordle.shouldEndGame()).toBe(true);
+  });
+  test("returns false if no guess has been made", () => {
+    const newWordle = new Wordle(1);
+    expect(newWordle.shouldEndGame()).toBe(false);
+  });
+  test("returns false if there are guesses left and the word has not been guessed", () => {
+    const newWordle = new Wordle();
+    newWordle.appendGuess("GUESS");
+    expect(newWordle.shouldEndGame()).toBe(false);
   });
 });
